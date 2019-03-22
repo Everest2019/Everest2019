1a331f5a8d1a6930ffc19becb4f2a0eb81a45eac
a-mandy <34047106+a-mandy@users.noreply.github.com>
2019-03-22 14:48:14 -0600

    arreglar punto api
    amanda

--- api/componentes/actividades/actividades.api.js
+++ api/componentes/actividades/actividades.api.js
@@ -1,53 +1,9 @@
 'use strict';
-const modelo_actividades = require('./actividades.model');
+let actividadSchema = new mongoose.Schema({
+    imagen: {type:String, required:true},
+    fecha: {type:Date, required:true},
+    titulo:{type:String,required:true},
+    descripcion:{type:String,required:true }
+});
 
-module.exports.registrar = (req, res) =>{
-    let nueva_actividad = new modelo_actividades(
-    {
-        fecha : req.body.fecha,
-        titulo : req.body.titulo,
-        descripcion : req.body.descripcion
-    }    
-  );
-  nueva_actividad.save(function(error){
-    if(error){
-       res.json( 
-           {
-               success : false,
-               msg : `No se pudo registrar la actividad`
-           }
-       ); 
-    }else{
-        res.json(
-            {
-                succes : true,
-                msg : `Se registro correctamente la actividad`
-            }
-        ); 
-        
-    } 
-      
-  });
-}
-module.exports.listar_todos = (req, res) =>{
-    modelo_actividades.find().then(
-        function(actividades){
-            if (actividades.length > 0){
-                res.json(
-                    {
-                        sucess:true,
-                        actividades: actividades
-                    }
-                )
-            }else{
-                res.json(
-                    {
-                        succes:false,
-                        actividades:'No se encontraron actividades'
-                    }
-                )
-            }
-        }
-    )
-}
-
+module.exports = mongoose.model('Actividades',actividadSchema);
