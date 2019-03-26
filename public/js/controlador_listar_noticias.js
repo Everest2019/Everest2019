7fe68d030ec4a7f78b91d8634e58f4adba9debb0
Amanda Campos Zeledon <acamposz@ucenfotec.ac.cr>
2019-03-26 13:15:00 -0600

    listar noticias

---
+++ public/js/controlador_listar_noticias.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const input_filtrar = document.querySelector('#txt_filtrar_noticia');
+const tabla= document.querySelector('#tbl_noticias tbody');
+
+let mostrar_noticias = () =>{
+  let noticias
+}
