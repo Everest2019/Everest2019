f35c178272b94ece8a8adc60b96e678bb83455ff
Amanda Campos Zeledon <acamposz@ucenfotec.ac.cr>
2019-03-27 00:38:06 -0600

    listar filtrar noticias

--- public/js/controlador_listar_noticias.js
+++ public/js/controlador_listar_noticias.js
@@ -3,6 +3,21 @@
 const input_filtrar = document.querySelector('#txt_filtrar_noticia');
 const tabla= document.querySelector('#tbl_noticias tbody');
 
+let listaNoticias = listar_noticias();
+mostrar_noticias();
+
+input_filtrar.addEventListener('keyup', mostrar_noticias);
+
 let mostrar_noticias = () =>{
-  let noticias
-}
+    tbody.innerHTML= '';
+    let filtro = input_filtrar.value;
+
+    for(let i=0;i<listaNoticias.length;i++){
+      if(listaNoticias[i]['titulo'].toLowerCase().includes(filtro.toLowerCase())){
+        let fila = tbody.insertRow();
+        fila.insertCell().innerHTML = listaNoticias[i]['fecha'];
+        fila.insertCell().innerHTML = listaNoticias[i]['titulo'];
+        fila.insertCell().innerHTML = listaNoticias[i]['descripcion'];
+      }
+    }
+};
