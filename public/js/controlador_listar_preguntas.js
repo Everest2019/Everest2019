b53a2a4d7c90e9633169ee6ed966935af4daf184
Amanda Campos Zeledon <acamposz@ucenfotec.ac.cr>
2019-03-27 21:10:32 -0600

    filtrar provisional

--- public/js/controlador_listar_preguntas.js
+++ public/js/controlador_listar_preguntas.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const input_filtrar = document.querySelector('#txt_filtrar_preguntas');
-const tbody = document.querySelector('#tbl_preguntas body');
+const tbody = document.querySelector('#tbl_preguntas tbody');
 
 
-let preguntas= new Array(3);
-preguntas[0]= ("es el universo infinito?","si, es infinito");
-preguntas[1]= ("existe la posibilidad de viajar atrás en al tiempo","no , aun no se ha logrado");
+let preguntas= new Array(2);
+preguntas[0]= new Array("es el universo infinito?","si, es infinito");
+preguntas[1]= new Array ("existe la posibilidad de viajar atrás en al tiempo","no , aun no se ha logrado");
 
 
 
@@ -15,11 +15,13 @@ let mostrar_preguntas = ()=>{
   let filtro= input_filtrar.value;
 
   for(let i=0;i<preguntas.length;i++){
-    let fila = tbody.insertRow();
-    if(preguntas[i][0].toLowerCase().includes(filtro.toLowerCase())){
+
+   if(preguntas[i][0].toLowerCase().includes(filtro.toLowerCase())){
+      let fila = tbody.insertRow();
       fila.insertCell().innerHTML = preguntas[i][0];
       fila.insertCell().innerHTML = preguntas[i][1];
-    }
+   }
 
   }
 };
+mostrar_preguntas();
