685e6e7814cafc5dca099205048bebaaa4b78b4a
jeremy99351 <jalvarec1@ucenfotec.ac.cr>
2019-03-30 13:39:17 -0600

    filtrar centros educativos

--- public/js/servicio_centro_educativos_cercanos.js
+++ public/js/servicio_centro_educativos_cercanos.js
@@ -1,75 +1,5 @@
 'use strict';
 
-let registrar_centro_educativo = (pnombre_institucion, pcorreo_institucion, pcedula_institucion, ptipo_institucion, ptipo_sistema, ptipo_colegio, pprovincia, pcanton, pdistrito, pdireccion_exacta, platitud, plongitud, pidioma, preligion, pensenanza, pdescripcion_institucion, preferencia_historica, pano_fundacion, pmatricula, pmensualidad, pportada, pgaleria, ptelefono, pfax, pweb, pfacebook, pinstagram, ptwitter, pyoutube, plogo, pdocumento, pprimer_nombre, psegundo_nombre, pprimer_apellido, psegundo_apellido, pcorreo_encargado, pdepartamento, ptelefono_encargado, pextension, pidentificacion, pfotografia_encargado, paprobado, pestado, pservicio_adicional, ptipo_usuario) =>{
-
-   let request = $.ajax({
-       url: "http://localhost:4000/api/registrar_centro_educativo",
-       method: "POST",
-       data: { 
-            nombre_comercial : pnombre_institucion,
-            cedula_juridica : pcedula_institucion,
-            tipo_institucion : ptipo_institucion,
-            modalidad : ptipo_sistema,
-            tipo_colegio : ptipo_colegio,
-            distrito : pdistrito,
-            canton : pcanton,
-            provincia : pprovincia,
-            direccion_exacta : pdireccion_exacta,
-            ubicacion_mapa_lat : platitud,
-            ubicacion_mapa_lng : plongitud,
-            ano_creacion : pano_fundacion,
-            referencia_historica : preferencia_historica,
-            informacion_general : pdescripcion_institucion,
-            telefono : ptelefono,
-            fax : pfax,
-            pagina_web : pweb,
-            correo : pcorreo_institucion,
-            facebook : pfacebook,
-            twitter : ptwitter,
-            instagram : pinstagram,
-            youtube : pyoutube,
-            logo: plogo,
-            matricula : pmatricula,
-            mensualidad : pmensualidad,
-            imagen_portada : pportada,
-            galeria : pgaleria,
-            informacion_adicional : pidioma,
-            documento: pdocumento,
-            primer_nombre : pprimer_nombre,
-            segundo_nombre : psegundo_nombre,
-            primer_apellido : pprimer_apellido,
-            segundo_apellido : psegundo_apellido,
-            correo_encargado : pcorreo_encargado,
-            departamento : pdepartamento,
-            telefono_encargado : ptelefono_encargado,
-            extension : pextension,
-            identificacion : pidentificacion,
-            fotografia_encargado: pfotografia_encargado,
-            aprobado : paprobado,
-            estado : pestado,
-
-            servicio_adicional: pservicio_adicional,
-            tipo_usuario: ptipo_usuario
-       },
-       contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-       dataType: "json"
-     });
-      
-     request.done(function( msg ) {
-
-       swal.fire({
-           type: 'success',
-           title: 'Centro Educativo agregado correctamente',
-           text: `El Centro Educativo ${pnombre_institucion} ha sido agregado correctamente`
-       });
-      
-     });
-      
-     request.fail(function( jqXHR, textStatus ) {
-      
-     }); 
-};
-
 let listar_instituciones = () => {
   let input_filtrar = [];
   let request = $.ajax({
@@ -118,4 +48,30 @@ function buscar_centro_educativo(pid_centro_educativo){
   });
 
   return centro_educativo;
+};
+
+ //Buscar padre familia
+ function buscar_padre_familia(pid_padre_familia){
+  let padre_familia = [];
+  $.ajax({
+      url: 'http://localhost:4000/api/buscar_padre_familia',
+      method: 'POST',
+      contentType: "application/x-www-form-urlencoded; charset=utf-8",
+      async: false,
+      data: {
+          id : pid_padre_familia
+      },
+      beforeSend: function beforeSend() {
+
+      },
+      success: function success(response) {
+        padre_familia = response;
+
+      },
+      error: function error(_error) {
+          console.log("Request fail error:" + _error);
+      }
+  });
+
+  return padre_familia;
 };
