3ca26f8376f5f347503b8b6774b4947f729507ec
mmasisp <mmasisp@ucenfotec.ac.cr>
2019-03-30 13:09:40 -0600

    Cambio de css
    Se añadio css

--- public/js/controlador_listar_preguntas.js
+++ public/js/controlador_listar_preguntas.js
@@ -1,44 +1,45 @@
 'use strict';
 const lista_preguntas = document.querySelector('#lista_preguntas');
 const input_buscar = document.querySelector('#txt_buscar');
-let id = localStorage.getItem('centro_educativo');
 
 
 
-let mostrar_datos = () =>{
-    let preguntas = listar();
-    let filtro = input_buscar.value;
-  /*  if(preguntas[i]['id_centro_educativo'].includes(id)){
-  }*/
 
-    for(let i = 1; i<=preguntas.length; i++){
+let mostrar_datos = () => {
+  let preguntas = listar();
+  let centro_educativo = localStorage.getItem('centro_educativo');
+
+
+  for (let i = 0; i < preguntas.length; i++) {
+    if (preguntas[i]['id_centro_educativo'].includes(centro_educativo)){
     /*  if(preguntas[i]['pregunta'].includes(filtro)){
         console.log(preguntas[i]['pregunta']);
       }*/
-        let pregunta_frecuente = document.createElement('div');
-        pregunta_frecuente.classList.add('pregunta');
+    let pregunta_frecuente = document.createElement('div');
+    pregunta_frecuente.classList.add('pregunta');
 
-        let nombre_pregunta = document.createElement('div');
-        nombre_pregunta.classList.add('nombre_pregunta');
+    let nombre_pregunta = document.createElement('div');
+    nombre_pregunta.classList.add('nombre_pregunta');
 
-        let texto_pregunta = document.createElement('p');
-        texto_pregunta.innerHTML = preguntas[i]['pregunta'];
+    let texto_pregunta = document.createElement('p');
+    texto_pregunta.innerHTML = preguntas[i]['pregunta'];
 
-        nombre_pregunta.appendChild(texto_pregunta);
+    nombre_pregunta.appendChild(texto_pregunta);
 
-        //Respuesta
-        let respuesta_pregunta = document.createElement('div');
-        respuesta_pregunta.classList.add('respuesta_pregunta');
+    //Respuesta
+    let respuesta_pregunta = document.createElement('div');
+    respuesta_pregunta.classList.add('respuesta_pregunta');
 
-        let texto_respuesta = document.createElement('p');
-        texto_respuesta.innerHTML = preguntas[i]['respuesta'];
+    let texto_respuesta = document.createElement('p');
+    texto_respuesta.innerHTML = preguntas[i]['respuesta'];
 
-        respuesta_pregunta.appendChild(texto_respuesta);
+    respuesta_pregunta.appendChild(texto_respuesta);
 
-        pregunta_frecuente.appendChild(nombre_pregunta);
-        pregunta_frecuente.appendChild(respuesta_pregunta);
+    pregunta_frecuente.appendChild(nombre_pregunta);
+    pregunta_frecuente.appendChild(respuesta_pregunta);
 
-        lista_preguntas.appendChild(pregunta_frecuente);
-    }
+    lista_preguntas.appendChild(pregunta_frecuente);
+  }
+}
 }
 mostrar_datos();
