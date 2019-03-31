d5ed9afc1b5931e8860c856cf9a7c2ed21784283
amoscoas <amoscoas@ucenfotec.ac.cr>
2019-03-31 13:52:27 -0600

    Ashley Moscoa
    Panel de control centro educativo

--- public/js/controlador_panel_centro_educativo.js
+++ public/js/controlador_panel_centro_educativo.js
@@ -1,16 +1,24 @@
 'use stric';
 const btn_agregar_noticia = document.querySelector('#btn_agregar_noticia');
 const btn_agregar_actividad = document.querySelector('#btn_agregar_actividad');
-const btn_agregar_lista_utiles = document.querySelector('#btn_agregar_lista_utiles');
+const btn_agregar_pregunta_frecuente = document.querySelector('btn_modificar_pregunta_frecuente') ;
+const btn_agregar_etiqueta = document.querySelector('btn_agregar_etiqueta')
  
-let agregar_lista_utiles = () =>{
-  window.location.href = './registrar_lista_utiles.html';
+
+let agregar_noticia = () =>{
+  window.location.href = './registrar_noticia.html';
 }
 let agregar_actividad = () =>{
   window.location.href = './agregar_actividades.html';
 }
-let agregar_noticia = () =>{
-    window.location.href = './registrar_noticia.html';
-};
+let agregar_pregunta = () =>{
+  window.location.href = './agregar_preguntas_frecuentes.html';
+}
+let agregar_etiqueta = () =>{
+  window.location.href = './registrar_etiqueta.html';
+}
 
+
+btn_agregar_noticia.addEventListener('click', agregar_noticia);
 btn_agregar_actividad.addEventListener('click', agregar_actividad);
+btn_agregar_pregunta_frecuente.addEventListener('click', agregar_pregunta)
