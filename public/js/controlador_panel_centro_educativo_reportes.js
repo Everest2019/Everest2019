cbe78d959d2d7073f97df6a76404309c7bd89637
mmasisp <mmasisp@ucenfotec.ac.cr>
2019-04-25 17:07:04 -0600

    Casos de uso
    Cambios en algunos casos de uso

--- public/js/controlador_panel_centro_educativo_reportes.js
+++ public/js/controlador_panel_centro_educativo_reportes.js
@@ -29,32 +29,3 @@ fila.insertCell().innerHTML = 'Permite visualizar la cantidad de visitas realiza
  icono_redirigir1.classList.add('fas', 'fa-arrow-alt-circle-right', 'fa-2x');
  boton_redirigir1.appendChild(icono_redirigir1);   
  celda_configuracion1.appendChild(boton_redirigir1);
-
-
- let fila2 = tabla.insertRow();
-
-fila2.insertCell().innerHTML = 'Reporte visitas';
-fila2.insertCell().innerHTML = 'Permite visualizar la cantidad de visitas realizadas por los padres de familia';
-
- // se crea una nueva celda para el boton de generar el reporte 
- let celda_configuracion2 = fila2.insertCell();
- let boton_redirigir2 = document.createElement('a');
- boton_redirigir2.href = 'reporte2.html';
- let icono_redirigir2 = document.createElement('i');
- icono_redirigir2.classList.add('fas', 'fa-arrow-alt-circle-right', 'fa-2x');
- boton_redirigir2.appendChild(icono_redirigir2);   
- celda_configuracion2.appendChild(boton_redirigir2);
-
- let fila3 = tabla.insertRow();
-
- fila3.insertCell().innerHTML = 'Reporte visitas';
- fila3.insertCell().innerHTML = 'Permite visualizar la cantidad de visitas realizadas por los padres de familia';
- 
-  // se crea una nueva celda para el boton de generar el reporte 
-  let celda_configuracion3 = fila3.insertCell();
-  let boton_redirigir3 = document.createElement('a');
-  boton_redirigir3.href = 'reporte2.html';
-  let icono_redirigir3 = document.createElement('i');
-  icono_redirigir3.classList.add('fas', 'fa-arrow-alt-circle-right', 'fa-2x');
-  boton_redirigir3.appendChild(icono_redirigir3);   
-  celda_configuracion3.appendChild(boton_redirigir3);
